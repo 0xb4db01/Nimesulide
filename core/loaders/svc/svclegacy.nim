bc1c4c68c22adac05af74a4c77e4345a2c60be84
0xb4db01 <tripz.san@gmail.com>
2026-07-27 21:24:36 +0200

    removing TODO from svc loader template as service name was handled finally

--- core/loaders/svc/svclegacy.nim
+++ core/loaders/svc/svclegacy.nim
@@ -7,9 +7,6 @@ import ../core/crypto/exor/xorro
 
 var key: string = {key}
 
-# TODO: the serice name is hardcoded. It would be nice to make it configurable
-# via JSON config, however I need to add logic that is specific for SVC.
-# For now, change it here in the template.
 var SERVICE_NAME: LPTSTR =  "{service_name}".LPTSTR
 var gSvcStatus: SERVICE_STATUS
 var gSvcStatusHandle: SERVICE_STATUS_HANDLE
